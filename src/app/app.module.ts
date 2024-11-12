15d596b947ed5ee35f19788a29412f964c8e2196
LucasB <lucasbauduin152@gmail.com>
2024-11-12 12:13:08 +0100

    Index : first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,34 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './core/components/header/header.component';
+import {RouterLink, RouterLinkActive, RouterModule} from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { routes } from './app.routes';
+import { LocationCardComponent } from './shared/components/location-card/location-card.component';
+import { CardsListComponent } from './shared/components/cards-list/cards-list.component';
+import { LocationDetailsComponent } from './location-details/location-details.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { CarouselImageComponent } from './carousel-image/carousel-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IndexComponent,
+    LocationCardComponent,
+    CardsListComponent,
+    LocationDetailsComponent,
+    FooterComponent,
+    CarouselImageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    RouterLink,
+    RouterLinkActive
   ],
   providers: [
     provideClientHydration()
