22c6e63187d12899bbca041d2d338039325934be
Alexandre <alexandre.delecloz@gmail.com>
2025-01-01 10:53:34 +0100

    Map-Research:4.1 FIX card debug remove

--- src/app/shared/components/location-card/location-card.component.ts
+++ src/app/shared/components/location-card/location-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Router} from '@angular/router';
 import {DestinationSearch} from '../../../core/models/DestinationSearch';
 
@@ -7,14 +7,10 @@ import {DestinationSearch} from '../../../core/models/DestinationSearch';
   templateUrl: './location-card.component.html',
   styleUrl: './location-card.component.scss'
 })
-export class LocationCardComponent implements OnChanges {
+export class LocationCardComponent {
   @Input() locationActivity!: DestinationSearch;
 
   constructor(private router: Router) {
-  }
-
-  ngOnChanges(): void {
-    console.log('Received locationActivity:', this.locationActivity); // Debug log
   }
 
   onContinue(id: number): void {
