fb164a74b54996c834b9c2da7ec30d7614e5a1d0
Alexandre <alexandre.delecloz@gmail.com>
2025-01-01 11:24:59 +0100

    Map-Research:4.2 IMP Most Popular

--- src/app/features/index/index.component.ts
+++ src/app/features/index/index.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import { Observable} from 'rxjs';
 import {DestinationSearch} from '../../core/models/DestinationSearch';
 import {SearchResult} from '../../core/models/SearchResult';
+import {SearchService} from '../../core/services/search.service';
 
 @Component({
   selector: 'app-index',
@@ -11,16 +12,23 @@ import {SearchResult} from '../../core/models/SearchResult';
 export class IndexComponent implements OnInit {
 
   destinations: DestinationSearch[] = [];
+  isSearchActive: boolean = false;
 
-  constructor() {
+  constructor(private searchService: SearchService) {
   }
 
   ngOnInit(): void {
+
+    const results$ = this.searchService.searchDestinations('', [], '', 1, 3);
+    results$.subscribe((results) => {
+      this.destinations = results.destinations;
+    });
   }
 
   onSearchResults(results$: Observable<SearchResult>): void {
     results$.subscribe((results) => {
       this.destinations = results.destinations;
+      this.isSearchActive = true;
     });
   }
 }
